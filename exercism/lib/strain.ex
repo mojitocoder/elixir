8f0d6de0349bee72eb167858a10d1165b342c42a
Quynh Nguyen <quynh.nguyen@simplybusiness.co.uk>
2017-11-20 22:37:40 +0000

    Strain fixed

--- exercism/lib/strain.ex
+++ exercism/lib/strain.ex
@@ -1,11 +1,11 @@
 defmodule Strain do
   def keep(list, predicate) do
-    Enum.reverse(keep(list, predicate, []))
+    keep(list, predicate, [])
   end
 
   defp keep([head | tail], predicate, accumulator) do
     if apply(predicate, [head]) do
-      keep(tail, predicate, [head | accumulator])
+      keep(tail, predicate, accumulator ++ [head])
     else
       keep(tail, predicate, accumulator)
     end
@@ -16,14 +16,14 @@ defmodule Strain do
   end
 
   def discard(list, predicate) do
-    Enum.reverse(discard(list, predicate, []))
+    discard(list, predicate, [])
   end
 
   defp discard([head | tail], predicate, accumulator) do
     if apply(predicate, [head]) do
       discard(tail, predicate, accumulator)
     else
-      discard(tail, predicate, [head | accumulator])
+      discard(tail, predicate, accumulator ++ [head])
     end
   end
 
