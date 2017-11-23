f8a46abf0473f47f83871c087ee4c5903f022194
Quynh Nguyen <quynh.nguyen@simplybusiness.co.uk>
2017-11-23 11:36:19 +0000

    Series done

--- exercism/lib/string_series.ex
+++ exercism/lib/string_series.ex
@@ -1,5 +1,13 @@
 defmodule StringSeries do
   def slices(string, len) do
-    
+    if len <= 0 do
+      []
+    else
+      string
+        |> String.to_charlist
+        |> Enum.with_index
+        |> Enum.filter(fn {_, index} -> index <= String.length(string) - len end)
+        |> Enum.map(fn {_, index} -> String.slice(string, index, len) end)
+    end
   end
 end
