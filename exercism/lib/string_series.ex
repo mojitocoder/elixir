714fb4f9faab653a7fe38fa1e2127bb57d8f8db4
Quynh Nguyen <quynh.nguyen@simplybusiness.co.uk>
2017-11-23 12:56:06 +0000

    Making Series more Elixir lang

--- exercism/lib/string_series.ex
+++ exercism/lib/string_series.ex
@@ -1,13 +1,13 @@
 defmodule StringSeries do
+  def slices(_, len) when len <= 0 do
+    []
+  end
+
   def slices(string, len) do
-    if len <= 0 do
-      []
-    else
-      string
-        |> String.to_charlist
-        |> Enum.with_index
-        |> Enum.filter(fn {_, index} -> index <= String.length(string) - len end)
-        |> Enum.map(fn {_, index} -> String.slice(string, index, len) end)
-    end
+    string
+      |> String.to_charlist
+      |> Enum.with_index
+      |> Enum.filter(fn {_, index} -> index <= String.length(string) - len end)
+      |> Enum.map(fn {_, index} -> String.slice(string, index, len) end)
   end
 end
