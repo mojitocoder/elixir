30a6978a95b86bebc0fff26509921ac6fd91c1f2
Quynh Nguyen <quynhnh@gmail.com>
2017-11-28 04:48:11 +0000

    In progress

--- exercism/lib/sum_of_multiples.ex
+++ exercism/lib/sum_of_multiples.ex
@@ -1,6 +1,20 @@
 defmodule SumOfMultiples do
-  def to(no, factors) when is_integer(no) do
-    #is_list(factors) && 
+  def to(no, factor) when is_integer(factor) do
+    div(factor * div(no, factor) * (div(no, factor) + 1), 2)
+  end
+
+  def to(no, factors) do
+
+
+    #is_list(factors) &&
+    #kn(n+1)/2
+    #To find n use 1000/3 = 333 + remainder, 1000/5 = 200 + remainder, 1000/15 = 66 + remainder
+    #sum multiples of 3: 3⋅333(333+1)/2=166833
+
     0
+  end
+
+  def find_combos([head | tail], accumulator) do
+
   end
 end
