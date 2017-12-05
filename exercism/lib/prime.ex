686c811f938431bfed6cf14143719805e236d695
Quynh Nguyen <quynh.nguyen@simplybusiness.co.uk>
2017-12-05 22:00:37 +0000

    Nth prime done

---
+++ exercism/lib/prime.ex
@@ -0,0 +1,42 @@
+defmodule Prime do
+
+  def nth(0), do: raise ArgumentError, message: "Invalid argument n"
+
+  def nth(1), do: 2
+
+  def nth(n) do
+    nth(n, 3, [2])
+      |> hd
+  end
+
+  def nth(n, max, primes) do
+    if Enum.count(primes) == n do
+      primes
+    else
+      if is_prime?(max) do
+        nth(n, max + 1, [max] ++ primes)
+      else
+        nth(n, max + 1, primes)
+      end
+    end
+  end
+
+  def is_prime?(n) do
+    if n in [2, 3] do
+      true
+    else
+      2..div(n, 2)
+          |> Stream.drop_while(fn i -> rem(n, i)!=0 end)
+          |> Stream.take(1)
+          |> Enum.count == 0
+    end
+  end
+
+  def infinite_stream(i \\ 1) do
+    [i] |> Stream.cycle
+  end
+
+  def infinitely_increasing_stream(i \\ 1, step \\ 1) do
+    infinite_stream(i) |> Stream.transform(0, fn n, acc -> {[n + acc], acc + step} end)
+  end
+end
