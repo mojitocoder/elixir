b7be3f1fc80d460add41e485c33ac3ec758e9d8f
Quynh Nguyen <quynhnh@gmail.com>
2017-12-08 21:09:28 +0000

    Binary done

--- exercism/lib/binary.ex
+++ exercism/lib/binary.ex
@@ -1,5 +1,17 @@
 defmodule Binary do
   def to_decimal(b) do
-    1
+    cond do
+      b <> "01"
+        |> String.graphemes
+        |> Enum.uniq
+        |> Enum.sort != ["0", "1"] ->
+        0
+      true ->
+        b |> String.graphemes
+          |> Enum.reverse
+          |> Enum.with_index
+          |> Enum.map(fn {n, i} -> String.to_integer(n) * (:math.pow(2, i) |> round) end)
+          |> Enum.sum
+    end
   end
 end
