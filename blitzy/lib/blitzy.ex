293bf9ce175807f873e392bd19767d646ae4d08c
Quynh Nguyen <quynhnh@gmail.com>
2018-01-21 19:59:17 +0000

    Print results

--- blitzy/lib/blitzy.ex
+++ blitzy/lib/blitzy.ex
@@ -5,5 +5,44 @@ defmodule Blitzy do
     1..n_workers
     |> Enum.map(fn _ -> Task.async(worker_fun) end)
     |> Enum.map(&Task.await(&1))
+    |> parse_results
+  end
+
+  def parse_results(results) do
+    {successes, _failures} =
+      results
+      |> Enum.split_with(fn x ->
+        case x do
+          {:ok, _} -> true
+          _ -> false
+        end
+      end)
+
+    total_workers = Enum.count(results)
+    total_success = Enum.count(successes)
+    total_failure = total_workers - total_success
+    data = successes |> Enum.map(fn {:ok, time} -> time end)
+    average_time = average(data)
+    longest_time = Enum.max(data)
+    shortest_time = Enum.min(data)
+
+    IO.puts("""
+    Total workers    : #{total_workers}
+    Successful reqs  : #{total_success}
+    Failed res       : #{total_failure}
+    Average (msecs)  : #{average_time}
+    Longest (msecs)  : #{longest_time}
+    Shortest (msecs) : #{shortest_time}
+    """)
+  end
+
+  defp average(list) do
+    sum = Enum.sum(list)
+
+    if sum > 0 do
+      sum / Enum.count(list)
+    else
+      0
+    end
   end
 end
